5f4605c503a4a80ed2876b544a09117f5eed6eba
Ioan Zicu <ioan.zicu@olx.pl>
2025-04-20 14:46:48 +0200

    push Null on stack when OpReturn

--- vm/vm.go
+++ vm/vm.go
@@ -341,6 +341,15 @@ func (vm *VM) Run() error {
 				return err
 			}
 
+		case code.OpReturn:
+			vm.popFrame()
+			vm.pop()
+
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
+
 		}
 	}
 

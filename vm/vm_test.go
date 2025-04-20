8b340b88b64c7f8f627a9f191eb5e4b3323780ae
Ioan Zicu <ioan.zicu@olx.pl>
2025-04-20 14:39:19 +0200

    executing function calls

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -302,3 +302,55 @@ func TestIndexExpessions(t *testing.T) {
 
 	runVmTests(t, tests)
 }
+
+func TestCallingFunctionsWithoutArguments(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let fivePlusTen = fn() { 5 + 10; };
+			fivePlusTen();
+ 			`,
+			expected: 15,
+		},
+		{
+			input: `
+			let one = fn() { 1; };
+			let two = fn() { 2; };
+			one() + two()
+ 			`,
+			expected: 3,
+		},
+		{
+			input: `
+			let a = fn() { 1 };
+			let b = fn() { a() + 1 };
+			let c = fn() { b() + 1 };
+			c()
+ 			`,
+			expected: 3,
+		},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestFunctionsWithReturnStatement(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let earlyExit = fn() { return 33, 66; };
+			earlyExit();
+			`,
+			expected: 33,
+		},
+		{
+			input: `
+			let earlyExit = fn() { return 33, return 66; };
+			earlyExit();
+			`,
+			expected: 33,
+		},
+	}
+
+	runVmTests(t, tests)
+}
